6732b0c9ab6ea2e190e18955f21707ae67e99190
Christopher Treviño <christophertrev@gmail.com>
2015-01-27 10:27:49 -0800

    Add Rotating Wheel

--- EmotiFaces-App/ViewController.swift
+++ EmotiFaces-App/ViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
-    @IBOutlet weak var emotionImage: UIImageView!
     
+    @IBOutlet weak var emotionImage: UIImageView!
+    @IBOutlet weak var emotionPicker: UIPickerView!
+    let pickerData = ["happy", "sad"];
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var unhappyString = "/Users/christophertrev/Desktop/latest.png";
+        let unhappyString = "/Users/christophertrev/Desktop/latest.png";
         emotionImage.image = UIImage(named:unhappyString);
+        emotionPicker.dataSource = self;
+        emotionPicker.delegate = self;
         
     }
 
@@ -25,7 +30,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+//    func viewForRow(row:Int, forComponent:Int)
+//    {
+//    float rate = [_exchangeRates[row] floatValue];
+//    float dollars = [_dollarText.text floatValue];
+//    float result = dollars * rate;
+    
+//    NSString *resultString = [[NSString alloc] initWithFormat:
+//    @"%.2f USD = %.2f %@", dollars, result,
+//    _countryNames[row]];
+//    _resultLabel.text = resultString;
+//    }
 
+    
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.pickerData.count;
+    }
+    
+    //MARK: Delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        myLabel.text = pickerData[row]
+    }
+    
 
+    
 }
 
