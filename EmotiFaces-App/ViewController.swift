85cac5e46d5cc020ab64f0e2afe9f7603f4534bc
Christopher Treviño <christophertrev@gmail.com>
2015-01-27 14:32:01 -0800

    Grabs image from local server

--- EmotiFaces-App/ViewController.swift
+++ EmotiFaces-App/ViewController.swift
@@ -22,11 +22,11 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let unhappyString = "/Users/christophertrev/Desktop/latest.png";
-        emotionImage.image = UIImage(named:unhappyString);
-
+//        let unhappyString = "/Users/christophertrev/Desktop/latest.png";
+//        emotionImage.image = UIImage(named:unhappyString);
+        emotionImage.contentMode = .ScaleAspectFit
         
-        var request = NSMutableURLRequest(URL: NSURL(string: "http://10.7.4.245:3000/emotion")!)
+        var request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:3000/emotion")!)
 //        var request = NSMutableURLRequest(URL: NSURL(string: "https://emotifaces.herokuapp.com/emotion")!)
         self.httpGet(request){
             (data, error) -> Void in
@@ -35,20 +35,48 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
                 println(error)
 //                println(error.localizedDescription)
             } else {
-                println(data)
-                var arr : [String];
-                
-                for (key: String, subJson: JSON) in data {
-                    //Do something you want
-                    println(key, subJson)
-                    self.pickerData.append(subJson.stringValue)
+//                println(data)
+                var result = JSON(data: data)
+                dispatch_sync(dispatch_get_main_queue(), { () -> Void in
+                    for (key: String, subJson: JSON) in result {
+                        //Do something you want
+                        println(key, subJson)
+                        self.pickerData.append(subJson.stringValue)
+                    }
+                    println(self.pickerData)
+                    self.emotionPicker.dataSource = self;
+                    self.emotionPicker.delegate = self;
                     
-                }
-                println(self.pickerData)
-                self.emotionPicker.dataSource = self;
-                self.emotionPicker.delegate = self;
+                })
+
 //                self.pickerData.
 //                self.pickerData = data.string!;
+            }
+        }
+        
+        var requestWithImage = NSMutableURLRequest(URL: NSURL(string: "http://localhost:3000/emotion/happy")!)
+        self.httpGet(requestWithImage){
+            (data, error) -> Void in
+            if error != nil {
+                println("error")
+                println(error)
+                //                println(error.localizedDescription)
+            } else {
+                dispatch_sync(dispatch_get_main_queue(), { () -> Void in
+                    println("loading image")
+                    var imageView = UIImageView(frame: self.emotionImage.bounds)
+                    imageView.contentMode = .ScaleAspectFill
+                    imageView.image =  UIImage(data: data)
+                    imageView.center = self.emotionImage.center
+                    self.emotionImage.addSubview(imageView)
+                    
+                
+                    
+//                    self.emotionImage.contentMode = .ScaleAspectFill
+//                    self.emotionImage.image = UIImage(data: data);
+//                    self.emotionImage
+                })
+                
             }
         }
         
@@ -81,18 +109,18 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
     
     
 
-    func httpGet(request: NSURLRequest!, callback: (JSON, String?) -> Void) {
+    func httpGet(request: NSURLRequest!, callback: (NSData, String?) -> Void) {
         var session = NSURLSession.sharedSession()
         var task = session.dataTaskWithRequest(request){
             (data, response, error) -> Void in
             if error != nil {
                 println(error.debugDescription)
-                callback("", error.localizedDescription)
+                callback(NSData(), error.localizedDescription)
             } else {
 //                var result = NSString(data: data, encoding:
 //                    NSASCIIStringEncoding)!
-                var result = JSON(data: data)
-                callback(result, nil)
+                
+                callback(data, nil)
             }
         }
         task.resume()
