d6dd4418c2462a11fbabb8538e7563bfcbb01b4b
Christopher Treviño <christophertrev@gmail.com>
2015-01-27 21:26:52 -0800

    Add camera Helping functions

--- EmotiFaces-App/CameraViewController.swift
+++ EmotiFaces-App/CameraViewController.swift
@@ -8,12 +8,35 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 
 class CameraViewController: UIViewController{
-
+    let captureSession = AVCaptureSession()
+    var captureDevice : AVCaptureDevice?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        
+        let devices = AVCaptureDevice.devices()
+        println(devices)
+        
+        // Loop through all the capture devices on this phone
+        for device in devices {
+            // Make sure this particular device supports video
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back camera
+                if(device.position == AVCaptureDevicePosition.Back) {
+                    captureDevice = device as? AVCaptureDevice
+                }
+            }
+        }
+        
+        if captureDevice != nil {
+            beginSession()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,4 +44,18 @@ class CameraViewController: UIViewController{
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func beginSession() {
+        var err : NSError? = nil
+        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
+        
+        if err != nil {
+            println("error: \(err?.localizedDescription)")
+        }
+        
+        var previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.view.layer.addSublayer(previewLayer)
+        previewLayer?.frame = self.view.layer.frame
+        captureSession.startRunning()
+    }
 }
