8f5846a98724b2eef349d60c7ad1e2796014abed
Christopher Treviño <christophertrev@gmail.com>
2015-01-27 21:14:38 -0800

    Add New tabbed views

---
+++ EmotiFaces-App/CameraViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CameraViewController.swift
+//  EmotiFaces-App
+//
+//  Created by Christopher Trevino on 1/27/15.
+//  Copyright (c) 2015 Christopher Trevino. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CameraViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
